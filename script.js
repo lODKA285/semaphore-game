045c67cec7eddf1cacdd7f7bfef4ea0f940f1116
lODKA285 <lida_shinkorenko@mail.ru>
2025-04-14 21:38:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startTraining() {
+  document.getElementById('game-area').innerHTML = 'Здесь будет обучение!';
+}
+
+function startGame() {
+  document.getElementById('game-area').innerHTML = 'Здесь будет игра!';
+}
+
+function startTimerMode() {
+  document.getElementById('game-area').innerHTML = 'Здесь будет режим на время!';
+}
